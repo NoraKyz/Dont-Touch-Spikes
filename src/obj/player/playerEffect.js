b075bd6c9e6abf2df8282832583c46df74b434f8
NgocMai102 <mai.pham.1022k3@gmail.com>
2023-07-03 16:23:22 +0700

    add particles

--- src/obj/player/playerEffect.js
+++ src/obj/player/playerEffect.js
@@ -1,48 +1,61 @@
 import { Texture } from "pixi.js";
 import { Emitter, upgradeConfig } from "@pixi/particle-emitter";
-import config from "../../../assets/aim/emitter.json"
+import config from "../../../assets/aim/emitter.json";
 import * as TWEEN from "@tweenjs/tween.js";
 
 export class PlayerEffect {
-    constructor(obj) {
-        this.obj = obj;
-        this._initEffect();
-        this.onReset();
+  constructor(obj) {
+    this.obj = obj;
+    this._initEffect();
+    this.onReset();
+  }
+
+  _initEffect() {
+    this._despawnEffect();
+    this._flyEffect();
+  }
+
+  _despawnEffect() {
+    this.despawnEffect = new TWEEN.Tween(this.obj).to({ alpha: 0 }, 2000);
+  }
+
+  _flyEffect() {
+    let texture = Texture.from("assets/images/HardCircle.png");
+    this.emitter = new Emitter(
+      this.obj.parent,
+      [texture],
+      upgradeConfig(config)
+    );
+    this.emitter.emit = true;
+  }
+
+  _updateParticles() {
+    this.emitter.updateSpawnPos(this.obj.position.x, this.obj.position.y);
+  }
+
+  _deadEffect(dt) {
+    if (this.enableDeadEffect == false) {
+      return;
     }
 
-    _initEffect() {
-        this._despawnEffect();
-        //this._flyEffect();
-    }
+    this.obj.rotation += 0.8 * dt;
+  }
 
-    _despawnEffect() {
-        this.despawnEffect = new TWEEN.Tween(this.obj)
-            .to({ alpha: 0 }, 2000)
-    }
+  onLose() {
+    this.enableDeadEffect = true;
+    this.despawnEffect.start();
+  }
 
-    _deadEffect(dt) {
-        if (this.enableDeadEffect == false) {
-            return;
-        }
+  onReset() {
+    this.enableDeadEffect = false;
+    this.despawnEffect.stop();
+    this.obj.alpha = 1;
+    this.obj.rotation = 0;
+  }
 
-        this.obj.rotation += 0.8 * dt;
-    }
-
-    onLose(){
-        this.enableDeadEffect = true;
-        this.despawnEffect.start();
-    }
-
-    onReset() {
-        this.enableDeadEffect = false;
-        this.despawnEffect.stop();
-
-        this.obj.alpha = 1;
-        this.obj.rotation = 0;
-    }
-
-    update(dt) {
-        this._deadEffect(dt);    
-        //this.emitter.update(dt);
-    }
+  update(dt) {
+    this._deadEffect(dt);
+    this.emitter.update(dt * 0.1);
+    this._updateParticles();
+  }
 }

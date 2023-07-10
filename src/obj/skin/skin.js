ded5b965291b5b8331fdd5164cbfe62e459caefa
Le Quy Long <longkold00@gmail.com>
2023-07-10 14:54:47 +0700

    + Add class skin
    Thông số skin

---
+++ src/obj/skin/skin.js
@@ -0,0 +1,15 @@
+
+export class Skin {
+    constructor() {     
+        this.texture1 = null;  // Default
+        this.texture2 = null; // Flying while playing
+        this.texture3 = null; // Flying while not playing
+        this.particle = null; // Particle effect
+
+        this.cost = {
+            type: null, // Type of currency
+            amount: null // Amount of currency
+        }
+        this.enabled = false; // Has the player unlocked this skin?
+    }
+}
